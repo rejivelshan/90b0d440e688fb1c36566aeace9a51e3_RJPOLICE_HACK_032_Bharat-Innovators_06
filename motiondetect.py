16ed480f6b08e8b1d307090cf5e5d19d2223a7b6
Rejiv Elshan Nify <128121228+rejivelshan@users.noreply.github.com>
2023-12-26 23:35:37 +0530

    Update and rename imagrecog.py to motiondetect.py

---
+++ motiondetect.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+
+
+ret, reference_frame = cap.read()
+reference_gray = cv2.cvtColor(reference_frame, cv2.COLOR_BGR2GRAY)
+
+
+displacement_threshold = 7000000
+
+while True:
+
+    ret, frame = cap.read()
+
+
+    current_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    frame_diff = cv2.absdiff(reference_gray, current_gray)
+
+
+    displacement = np.sum(frame_diff)
+
+
+    if displacement > displacement_threshold:
+
+        print("Camera displacement detected!")
+
+       
+        reference_frame = frame.copy()
+        reference_gray = current_gray.copy()
+
+
+    cv2.imshow('Camera Displacement Detection', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
